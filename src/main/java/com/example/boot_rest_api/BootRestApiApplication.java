472067a07c849471e60ef65e28df4321886bd66a
Wencan Peng <p.wencan@outlook.com>
2024-08-03 01:15:38 +1000

    Initial commit: dependencies include spring web, lombok, spring boot dev tools, junit jupiter, mockito

---
+++ src/main/java/com/example/boot_rest_api/BootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.boot_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestApiApplication.class, args);
+	}
+
+}
