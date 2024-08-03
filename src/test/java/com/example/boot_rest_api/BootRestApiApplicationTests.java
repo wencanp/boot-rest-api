c12294c1e6aa26642f51d03e551e0e2ff512eae7
Wencan Peng <p.wencan@outlook.com>
2024-08-03 23:03:34 +1000

    Runnable for GET all employees: add dependency and mute test

--- src/test/java/com/example/boot_rest_api/BootRestApiApplicationTests.java
+++ src/test/java/com/example/boot_rest_api/BootRestApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BootRestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
