e5a01df7df8a3b09e767be2b39293a293a3d7451
Wencan Peng <p.wencan@outlook.com>
2024-08-03 23:10:23 +1000

    Modify EmployeeController: implement POST single employee

--- src/main/java/com/example/boot_rest_api/EmployeeController.java
+++ src/main/java/com/example/boot_rest_api/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.example.boot_rest_api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -13,5 +15,16 @@ public class EmployeeController {
     @GetMapping(produces = "application/json")
     public Employees getAllEmployees() {
         return employeeManager.getAllEmployees();
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee) {
+        employeeManager.addEmployee(employee);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(employee.getEmployee_id())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
