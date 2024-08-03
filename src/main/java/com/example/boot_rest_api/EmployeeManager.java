906cd8107eea1e8954a11bd85d973ec1d383660c
Wencan Peng <p.wencan@outlook.com>
2024-08-03 22:02:35 +1000

    Add EmployeeManager.class: add static employees, get employee list func, and add single employee func

---
+++ src/main/java/com/example/boot_rest_api/EmployeeManager.java
@@ -0,0 +1,38 @@
+package com.example.boot_rest_api;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmployeeManager {
+    private static Employees list = new Employees();
+
+    //  Initialize service and fill with several employee entity
+    static {
+        list.getEmployeeList().add(
+                new Employee("1",
+                        "John",
+                        "Doe",
+                        "john.doe@example.com",
+                        "Developer"));
+        list.getEmployeeList().add(
+                new Employee("2",
+                        "Jane",
+                        "Smith",
+                        "jane.smith@example.com",
+                        "Designer"));
+        list.getEmployeeList().add(
+                new Employee("3",
+                        "Jim",
+                        "Beam",
+                        "jim.beam@example.com",
+                        "Manager"));
+    }
+
+    public Employees getAllEmployees() {
+        return list;
+    }
+
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
