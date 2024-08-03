3705d8e25c5909c6361d211c2e47c39889f14def
Wencan Peng <p.wencan@outlook.com>
2024-08-03 22:54:25 +1000

    Add EmployeeController.java

---
+++ src/main/java/com/example/boot_rest_api/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.example.boot_rest_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/employees")
+public class EmployeeController {
+    @Autowired EmployeeManager employeeManager;
+
+    @GetMapping(produces = "application/json")
+    public Employees getAllEmployees() {
+        return employeeManager.getAllEmployees();
+    }
+}
